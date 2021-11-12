9d718b0eaf4b19fb825528fff5648825d1ba6e55
Alison Saia <allie@moka.local>
2021-11-12 17:21:24 -0500

    Add vscode stuff

--- link.sh
+++ link.sh
@@ -1,5 +1,13 @@
 # Symlink dotfiles into their correct locations on macOS
 DIR=`pwd`
 cd $HOME
+
+# zsh
 ln -sf $DIR/.zshrc $HOME/.zshrc
 
+# vscode
+mkdir -p $DIR/vscode/snippets
+ln -sf $DIR/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+ln -sf $DIR/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -sf $DIR/vscode/snippets/ $HOME/Library/Application\ Support/Code/User
+ln -sf $DIR/vscode/.vscode $HOME/.vscode
