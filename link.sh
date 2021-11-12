a080c7fba571d7ed7fa1852b23d50e4eaec3da8f
Alison Saia <allie@saia.me>
2021-11-12 17:22:36 -0500

    Remove .vscode linking

--- link.sh
+++ link.sh
@@ -10,4 +10,3 @@ mkdir -p $DIR/vscode/snippets
 ln -sf $DIR/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 ln -sf $DIR/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
 ln -sf $DIR/vscode/snippets/ $HOME/Library/Application\ Support/Code/User
-ln -sf $DIR/vscode/.vscode $HOME/.vscode
